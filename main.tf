7a69301dd509e3f9b38bd5cf6372f94d90e6b020
root <dominic.routley@devopsgroup.com>
2020-04-28 09:59:43 +0000

    Tue Apr 28 2020 - Committed from Codeherent platform

--- main.tf
+++ main.tf
@@ -4,34 +4,32 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_virtual_machine" "test" {
-  name                  = "${var.prefix}-virtual_machine"
-  resource_group_name   = azurerm_resource_group.test.name
-  location              = var.location
-  network_interface_ids = [azurerm_network_interface.test.id]
-  vm_size               = var.vm_size
-
+  name                = "test-virtual_machine"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = var.location
+  network_interface_ids = [
+    azurerm_network_interface.test.id,
+  ]
+  vm_size = var.vm_size
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
       key_data = file("server.pub")
-      path = "/home/${var.os_user}/.ssh/authorized_keys"
+      path     = "/home/${var.os_user}/.ssh/authorized_keys"
     }
   }
-
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
-
   storage_os_disk {
     name              = "${var.prefix}-OS_disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
     computer_name  = var.computer_name
     admin_username = var.os_user
--- networking.tf
+++ networking.tf
@@ -1,15 +1,16 @@
 resource "azurerm_virtual_network" "test" {
   name                = "${var.prefix}-virtual_network"
   resource_group_name = azurerm_resource_group.test.name
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  address_space = [
+    "10.0.0.0/16",
+  ]
+  location = var.location
 }
 
 resource "azurerm_network_interface" "test" {
   name                = "${var.prefix}-network_interface"
   resource_group_name = azurerm_resource_group.test.name
   location            = var.location
-
   ip_configuration {
     name                          = "${var.prefix}-address"
     public_ip_address_id          = azurerm_public_ip.test.id
